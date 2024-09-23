8ae5298db490db6b6ac84bf63e19f87bd2ac76a3
jayminbahtti <bhattijaymin@gmail.com>
2024-09-23 22:04:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// window.addEventListener('load', function () {
+//     var mobNav = document.getElementById('mobile-nav');
+//     mobNav.addEventListener('click', function () {
+//       this.classList.toggle('opened');
+//     }, false);
+//   }, false);
+
+
+  function myFunction() {
+    var element = document.getElementById("myDIV");
+    element.classList.toggle("active");
+  }
